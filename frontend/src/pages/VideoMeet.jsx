a05afbc0ac16e8fbe0dcc8c4ae2b11355bd788d2
Sarthak-shastrakar-88 <sarthakshastrakar9@gmail.com>
2025-09-26 16:53:53 +0530

    Update VideoMeet.jsx

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -456,7 +456,7 @@ export default function VideoMeetComponent() {
       let tracks = localVideoref.current.srcObject.getTracks();
       tracks.forEach((track) => track.stop());
     } catch (e) {}
-    window.location.href = "/home";
+    window.location.href = "/";
   };
 
   let openChat = () => {
